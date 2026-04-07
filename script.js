34a657d66dfaddd2063aa74f1667b0bc9842b62c
Masato Azechi <masato.azechi@we-re.net>
2026-04-07 10:51:39 +0900

    とりあえず直した

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const targets = document.querySelectorAll(".fade-in");
 const animTargets = document.querySelectorAll(".anim");
 
@@ -30,36 +29,3 @@ const animObserver = new IntersectionObserver(
 
 targets.forEach(target => observer.observe(target));
 animTargets.forEach(target => animObserver.observe(target));
-=======
-const targets = document.querySelectorAll(".fade-in");
-const animTargets = document.querySelectorAll(".anim");
-
-const observer = new IntersectionObserver(
-  (entries) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("show");
-      }
-    });
-  },
-  {
-    threshold: 0.2
-  }
-);
-
-const animObserver = new IntersectionObserver(
-  (entries) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("is-animated");
-      }
-    });
-  },
-  {
-    threshold: 0.2
-  }
-);
-
-targets.forEach(target => observer.observe(target));
-animTargets.forEach(target => animObserver.observe(target));
->>>>>>> 37322146951dc1c0a607d789b75ef1fe022621d2
